c6c43e029ea41c58d26d7f9ef8f894719717c807
王超9 <wangchao9@le.com>
2017-05-15 10:49:40 +0800

    加入demo工程

---
+++ CodeStyleDemo/CodeStyleDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CodeStyleDemo
+//
+//  Created by wangchao9 on 2017/5/15.
+//  Copyright © 2017年 wangchao9. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CodeStyleDemo/CodeStyleDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CodeStyleDemo
+//
+//  Created by wangchao9 on 2017/5/15.
+//  Copyright © 2017年 wangchao9. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
