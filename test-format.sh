ecec56e0d0de637753fa2f2c002b7343748bcda2
王超9 <wangchao9@le.com>
2017-05-17 10:48:48 +0800

    支持指定Source和Target

--- test-format.sh
+++ test-format.sh
@@ -8,12 +8,23 @@ readonly script_path="${BASH_SOURCE[0]}"
 readonly script_dir="$( cd "$( dirname "${script_path}" )" && pwd )"
 
 CMD="$HOME/Library/Application Support/Developer/Shared/Xcode/Plug-ins/UncrustifyPlugin.xcplugin/Contents/Resources/uncrustify"
+SOURCE="$1"
+TARGET="$2"
 
-rm -rf "${script_dir}/Output"
-cp -rf "${1}" "${script_dir}/Output"
+if [[ -z $SOURCE ]]; then
+    exit -1;
+fi
 
-FILES="$(find "${script_dir}/Output" -name "*.[h,hpp,pch,c,cpp,m,mm]" -not -path "./3rd*" -not -path "./lib*" -not -path "./NA*" -not -path "./sharepods*" -not -path "**.framework**" -not -path "**Lib/*" -not -path "**Pods/*")"
+if [[ -z $TARGET ]]; then
+    TARGET="${script_dir}/Output"
+fi
+
+
+rm -rf "${TARGET}"
+cp -rf "${SOURCE}" "${TARGET}"
+
+FILES="$(find "${TARGET}" -name "*.[h,hpp,pch,c,cpp,m,mm]" -not -path "./3rd*" -not -path "./lib*" -not -path "./NA*" -not -path "./sharepods*" -not -path "**.framework**" -not -path "**Lib/*" -not -path "**Pods/*")"
 
 while read -r line;do
-    "$CMD" -c "${script_dir}/uncrustify_rebase.cfg" -l OC --no-backup "$line"
+    "$CMD" -c "${script_dir}/uncrustify_oc.cfg" -l OC --no-backup "$line"
 done <<< "$FILES"
