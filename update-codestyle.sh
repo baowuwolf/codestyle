7b552a80b4635198c033bb634999d3ffd03f14da
王超9 <wangchao9@le.com>
2017-05-18 10:27:22 +0800

    添加update方法

---
+++ update-codestyle.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#
+set -o errexit
+set -o pipefail
+set -o nounset
+
+readonly script_path="${BASH_SOURCE[0]}"
+readonly script_dir="$( cd "$( dirname "${script_path}" )" && pwd )"
+
+CMD="$HOME/Library/Application Support/Developer/Shared/Xcode/Plug-ins/UncrustifyPlugin.xcplugin/Contents/Resources/uncrustify"
+SOURCE="$1"
+TARGET="$SOURCE"
+
+if [[ -z $SOURCE ]]; then
+    exit -1;
+fi
+
+#if [[ -z $TARGET ]]; then
+#    TARGET="${script_dir}/Output"
+#fi
+
+
+# rm -rf "${TARGET}"
+# cp -rf "${SOURCE}" "${TARGET}"
+
+FILES="$(find "${TARGET}" -name "*.[h,hpp,pch,c,cpp,m,mm,pch]" -not -path "./3rd*" -not -path "./lib*" -not -path "./NA*" -not -path "./sharepods*" -not -path "**.framework**" -not -path "**Lib/*" -not -path "**Pods/*" -not -path "*/**/Vendors/*")"
+
+while read -r line;do
+    "$CMD" -c "${script_dir}/uncrustify.cfg" -l OC --no-backup "$line"
+done <<< "$FILES"
